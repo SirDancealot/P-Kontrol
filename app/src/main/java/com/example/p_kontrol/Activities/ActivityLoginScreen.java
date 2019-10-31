51a440e7f59e8842e531f13e74c4836fe9a6967c
pc <valdemar_hh@hotmail.com>
2019-10-31 12:19:03 +0100

    navigation added.

--- app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
+++ app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
@@ -2,15 +2,43 @@ package com.example.p_kontrol.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.p_kontrol.R;
 
-public class ActivityLoginScreen extends AppCompatActivity {
+public class ActivityLoginScreen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.login_screen_scene1);
+
+        Button login = findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == findViewById(R.id.login_button)){
+            setContentView(R.layout.activity_login_screen);
+        }
+        if (view == findViewById(R.id.login_button)){
+            Intent mapView_intent = new Intent(this, ActivityMapView.class);
+            startActivity(mapView_intent);
+        }
+
     }
 }
--- app/src/main/java/com/example/p_kontrol/Activities/MainActivity.java
+++ app/src/main/java/com/example/p_kontrol/Activities/MainActivity.java
@@ -13,11 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Switch from splash to log-in
+        splashTransition();
+
+
+
+
+
+
         //ChangeActivity_MapView();
     }
 
     public void ChangeActivity_MapView() {
         Intent mapView_intent = new Intent(MainActivity.this, ActivityMapView.class);
         startActivity(mapView_intent);
+    }
+
+    public void splashTransition() {
+        Intent login_intent = new Intent(MainActivity.this, ActivityLoginScreen.class);
+        startActivity(login_intent);
+        //OverridePendingTransition(R.anim.fade_in, R.anim.slide_up);
+
     }
 }
