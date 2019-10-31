7c9ef20dacf6b7c7d811b2135c1e950490d98d35
bulgurgm2 <s165372@student.dtu.dk>
2019-10-31 16:22:20 +0100

    add bubbleTip

--- app/src/main/java/com/example/p_kontrol/Fragments/FragTipBobble.java
+++ app/src/main/java/com/example/p_kontrol/Fragments/FragTipBobble.java
@@ -9,66 +9,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.p_kontrol.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragTipBobble.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragTipBobble#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragTipBobble extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class FragTipBobble extends Fragment implements View.OnClickListener {
 
-    private OnFragmentInteractionListener mListener;
+
+    private TextView readMore;
+    private TextView tip;
+    private CircleImageView tipPerson;
+    private String string;
+    private View view;
+    private boolean dragState;
+
+    private View con;
+
+    private FragBottomMenu.OnFragmentInteractionListener mListener;
+
 
     public FragTipBobble() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragTipBobble.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragTipBobble newInstance(String param1, String param2) {
-        FragTipBobble fragment = new FragTipBobble();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tip_bobble, container, false);
+        view = inflater.inflate(R.layout.fragment_tip_bobble, container, false);
+
+
+
+
+        tip = (TextView) view.findViewById(R.id.bobbelTiptextView);
+        readMore = (TextView) view.findViewById(R.id.bobbelTipreadMore);
+        readMore.setOnClickListener(this);
+        con = view.findViewById(R.id.fagTip);
+        con.setOnClickListener(this);
+        tipPerson = (CircleImageView) view.findViewById(R.id.bobbelTipImg);
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.dontAnimate();
+        Glide.with(FragTipBobble.this).load(R.drawable.tipprofileimg).into(tipPerson);
+        string = getString(R.string.tip1);
+        if (string.length() > 65)
+        {
+            string = string.substring(0, 65) + "...";
+        }
+        tip.setText(string);
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -78,21 +81,27 @@ public class FragTipBobble extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == con){
+            con.setVisibility(View.GONE);
+        } else if (v == readMore){
+            readMore.setText("ikke lavet endnu");
+        }
+
+    }
+
+
+    public void showTip(){
+        con.setVisibility(View.VISIBLE);
     }
 
     /**
