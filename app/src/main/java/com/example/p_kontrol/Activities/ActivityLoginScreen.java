a20f0b34b8c9363e40012ea20bbbde0b28fe754b
Grobaxgrappleblast <hansbyager@gmail.com>
2019-10-31 15:46:25 +0100

    Merge branch 'ViewDevelopment' of C:\Users\Grobax\AndroidStudioProjects\P-Kontrol with conflicts.

--- app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
+++ app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
@@ -4,41 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.p_kontrol.R;
 
-public class ActivityLoginScreen extends AppCompatActivity implements View.OnClickListener {
+public class ActivityLoginScreen extends AppCompatActivity{
+
+    // Login Screen 1.
+    Button screen1_loginBtn;
+    Button screen1_SignUpBtn;
+
+    // Login Screen 2.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen_scene1);
 
-        Button login = findViewById(R.id.login_button);
+        // Screen 1's elements
+        screen1_loginBtn = findViewById(R.id.LoginScreen_1_LoginBtn);
+        screen1_SignUpBtn = findViewById(R.id.LoginScreen_1_SignUpBtn);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        setupListeners();
+    }
 
 
+    private void setupListeners(){
+        // Screen 1
+        screen1_loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen1_loginBtn(v);
+            }
+        });
 
+        screen1_SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen1_SignInBtn(v);
             }
         });
 
 
-    }
 
-    @Override
-    public void onClick(View view) {
-        if (view == findViewById(R.id.login_button)){
-            setContentView(R.layout.activity_login_screen);
-        }
-        if (view == findViewById(R.id.login_button)){
-            Intent mapView_intent = new Intent(this, ActivityMapView.class);
-            startActivity(mapView_intent);
-        }
 
     }
+
+    // Btn Methods for Screen 1.
+    private void screen1_loginBtn(View v){
+        Log.v("screen 1","Login btn clicked \n");
+
+        // todo Create a Scene shift.
+    }
+    private void screen1_SignInBtn(View v){
+        Log.v("screen 1","Sign in btn clicked \n");
+
+    }
+
+    // Btn Methods for Screen 2.
+
+
 }
