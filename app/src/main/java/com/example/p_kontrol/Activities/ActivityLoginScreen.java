d120f8f004351dccaac5db8cb4637fca5c40e6df
Grobaxgrappleblast <hansbyager@gmail.com>
2019-10-31 17:17:45 +0100

    merge

--- app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
+++ app/src/main/java/com/example/p_kontrol/Activities/ActivityLoginScreen.java
@@ -4,37 +4,103 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.p_kontrol.R;
 
-public class ActivityLoginScreen extends AppCompatActivity implements View.OnClickListener {
+public class ActivityLoginScreen extends AppCompatActivity{
+
+    // Login Screen 1.
+    Button screen1_loginBtn;
+    Button screen1_SignUpBtn;
+
+    // Login Screen 2.
+    Button screen2_loginManual;
+    Button screen2_loginFaceB;
+    Button screen2_loginGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen_scene1);
-        Button login = findViewById(R.id.login_button);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        // Screen 1's elements
+        screen1_loginBtn = findViewById(R.id.LoginScreen_1_LoginBtn);
+        screen1_SignUpBtn = findViewById(R.id.LoginScreen_1_SignUpBtn);
+
+        setupListeners_Screen1();
+    }
+
+
+    private void setupListeners_Screen1(){
+        screen1_loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen1_loginBtn(v);
+            }
+        });
+
+        screen1_SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen1_SignInBtn(v);
+            }
+        });
+
+    }
+    private void setupListeners_Screen2(){
+        screen2_loginManual.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen2_loginManual(v);
+            }
+        });
+
+        screen2_loginFaceB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen2_loginFaceB(v);
+            }
+        });
+
+        screen2_loginGoogle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                screen2_loginGoogle(v);
             }
         });
 
     }
 
-    // todo ?? hvad er dette view == ... det lyder sært.
-    @Override
-    public void onClick(View view) {
-        if (view == findViewById(R.id.login_button)){
-            setContentView(R.layout.activity_login_screen);
-        }
-        if (view == findViewById(R.id.login_button)){
-            Intent mapView_intent = new Intent(this, ActivityMapView.class);
-            startActivity(mapView_intent);
-            overridePendingTransition(R.anim.fade_in, R.anim.slide_up);
-        }
+    // Btn Methods for Screen 1.
+    private void screen1_loginBtn(View v){
+        Log.v("screen 1","Login btn clicked \n");
+        setContentView(R.layout.activity_login_screen);
+
+        screen2_loginManual = findViewById(R.id.LoginScreen_2_SignIn)       ;
+        screen2_loginFaceB  = findViewById(R.id.LoginScreen_2_LogFaceBook)  ;
+        screen2_loginGoogle = findViewById(R.id.LoginScreen_2_LogGoogle)    ;
+
+        setupListeners_Screen2();
+
+    }
+    private void screen1_SignInBtn(View v){
+        Log.v("screen 1","Sign in btn clicked \n");
+    }
+
+    // Btn Methods for Screen 2.
+    private void screen2_loginManual(View v){
+        Log.v("screen 2","login Manual btn clicked \n");
+        changeto_Activiy_MapView();
+    }
+    private void screen2_loginFaceB(View v){
+        Log.v("screen 2","login FaceBook btn clicked \n");
+        changeto_Activiy_MapView();
+    }
+    private void screen2_loginGoogle(View v){
+        Log.v("screen 2","login Google btn clicked \n");
+        changeto_Activiy_MapView();
+    }
+
+    private void changeto_Activiy_MapView(){
+        Intent changeActivity = new Intent(this, ActivityMapView.class);
+        startActivity(changeActivity);
     }
 }
