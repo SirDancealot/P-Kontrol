1e87eea12252b988e0b57895c2b20ec009329314
neezoy <valdemar_hh@hotmail.com>
2019-11-21 16:49:04 +0100

    Interfaces for -User -Tip -Backend -Database
    and their respective class skeletons

---
+++ app/src/main/java/com/example/p_kontrol/Backend/IDatabase.java
@@ -0,0 +1,17 @@
+package com.example.p_kontrol.Backend;
+
+import com.example.p_kontrol.UI.Services.ITipDTO;
+import com.example.p_kontrol.UI.Services.IUserDTO;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public interface IDatabase {
+
+    List<ITipDTO> getTipList(LatLng location, double radius);
+
+    IUserDTO getUser(id id);
+
+
+
+}
---
+++ app/src/main/java/com/example/p_kontrol/UI/Services/IBackend.java
@@ -0,0 +1,38 @@
+package com.example.p_kontrol.UI.Services;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public interface IBackend {
+
+
+    //Tips
+
+    List<ITipDTO> getTips(LatLng location);
+
+     void createTip(ITipDTO tip);
+
+     void rateTip(int star, ITipDTO tip);
+
+     boolean checkPAlert(LatLng location);
+
+     boolean isNewTipAvalible();
+
+     
+     //User
+
+    IUserDTO getUser(id id);
+
+
+
+
+
+
+
+
+
+
+
+
+}
