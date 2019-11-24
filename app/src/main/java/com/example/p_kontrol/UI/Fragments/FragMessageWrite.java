f37d994a93ec527621a7a55c2f9dc8aecc5bcc41
Grobaxgrappleblast <hansbyager@gmail.com>
2019-11-24 18:06:37 +0100

    - commit before Checkout ( working on Clickability of WriteTip, and Removing Keyboard when swiping)

--- app/src/main/java/com/example/p_kontrol/UI/Fragments/FragMessageWrite.java
+++ app/src/main/java/com/example/p_kontrol/UI/Fragments/FragMessageWrite.java
@@ -58,6 +58,22 @@ public class FragMessageWrite extends Fragment  implements View.OnClickListener 
 
         WriteTipAdapter adapter = new WriteTipAdapter(getChildFragmentManager(),fragmentList);
         ContentContainer.setAdapter(adapter);
+        ContentContainer.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
         return view;
