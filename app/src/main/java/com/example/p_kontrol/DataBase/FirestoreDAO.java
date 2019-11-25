56fee7a75ce1157c70de49c2f4b17fa45566045f
Mads Møller Jensen <mads1606@gmail.com>
2019-11-25 08:52:49 +0100

    Implemented the create tip method in FirestoreDAO

--- app/src/main/java/com/example/p_kontrol/DataBase/FirestoreDAO.java
+++ app/src/main/java/com/example/p_kontrol/DataBase/FirestoreDAO.java
@@ -6,22 +6,32 @@ import com.example.p_kontrol.Backend.IDatabase;
 import com.example.p_kontrol.UI.Services.ITipDTO;
 import com.example.p_kontrol.UI.Services.IUserDTO;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+
+import androidx.annotation.NonNull;
 
 public class FireStoreDAO implements IDatabase {
     String TAG = "FirestoreDAO";
     FirebaseFirestore fireDB = FirebaseFirestore.getInstance();
+    CollectionReference tips = fireDB.collection("tips");
+
+
 
     @Override
     public List<ITipDTO> getTipList(LatLng location, double radius) {
-        Task<com.google.firebase.firestore.QuerySnapshot> query =
-            fireDB.collection("tips").get();
+        Task<QuerySnapshot> query = tips.get();
 
         try {
              Tasks.await(query);
@@ -33,11 +43,25 @@ public class FireStoreDAO implements IDatabase {
             Log.w(TAG, "getTipList: ", query.getException());
         }
 
-        return new ArrayList<>(query.getResult().toObjects(TipDTO.class));
+        return new ArrayList<>(
+                Objects.requireNonNull( //Makes sure that a null pointer exception is avoided (recommended by IntelliJ)
+                    query.getResult() ).toObjects(TipDTO.class)
+        );
     }
 
     @Override
     public void createTip(ITipDTO tip) {
+        String id = tip.getAuthor() + "-" + System.currentTimeMillis();
+        tips.document(id).set(tip)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "createTip: tip \"" + id + "\" added to database"))
+                .addOnCompleteListener(task -> {
+                    if (task.isCanceled()) {
+                        Log.w(TAG, "createTip: Canceled with message: ", task.getException());
+                    } else if (!task.isSuccessful()) {
+                        Log.w(TAG, "createTip: Failed with message: ", task.getException());
+                    }
+                })
+        ;
 
     }
 
