610d4b98be5e5791f8fdf2c64e576d1ea1d8d5db
Emil Johannes Lundqvist <emlunde@gmail.com>
2020-01-10 09:54:52 +0100

    readTip ScrollView added and sized properly

---
+++ app/src/main/java/com/example/p_kontrol/Util/MaxHeightLinearLayout.java
@@ -0,0 +1,49 @@
+package com.example.p_kontrol.Util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+
+import com.example.p_kontrol.R;
+
+//https://gist.github.com/dominicthomas/1a268f3a113b490f751d9fb30cdb5875
+public class MaxHeightLinearLayout extends LinearLayout {
+
+    private int maxHeightDp;
+
+    public MaxHeightLinearLayout(Context context) {
+        super(context);
+    }
+
+    public MaxHeightLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        final TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MaxHeightLinearLayout, 0, 0);
+        try {
+            maxHeightDp = a.getInteger(R.styleable.MaxHeightLinearLayout_maxHeightDp, 0);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    public MaxHeightLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        int maxHeightPx = Math.round(maxHeightDp * density);
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeightPx, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void setMaxHeightDp(int maxHeightDp) {
+        this.maxHeightDp = maxHeightDp;
+        invalidate();
+    }
+
+}
