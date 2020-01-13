79ea3cf6a7c3c816f588f5b0e36131b791301e4f
Emil Johannes Lundqvist <emlunde@gmail.com>
2020-01-13 16:30:49 +0100

    new feedback underway (facebook group)

--- app/src/main/java/com/example/p_kontrol/UI/Feedback/ActivityFeedback.java
+++ app/src/main/java/com/example/p_kontrol/UI/Feedback/ActivityFeedback.java
@@ -2,6 +2,9 @@ package com.example.p_kontrol.UI.Feedback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,10 +20,59 @@ import com.example.p_kontrol.R;
 
 public class ActivityFeedback extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
-    IBackend backend = new Backend();
-    Spinner dropDownCategory;
-    EditText feedbackText;
-    Button sendFeedbackBtn;
+//    IBackend backend = new Backend();
+//    Spinner dropDownCategory;
+//    EditText feedbackText;
+//    Button sendFeedbackBtn;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_feedback);
+//
+//
+//        dropDownCategory = findViewById(R.id.feedbackDropDownMenu);
+//        ArrayAdapter<CharSequence> adapter =
+//                ArrayAdapter.createFromResource(this, R.array.feedback_categories_array,
+//                        R.layout.support_simple_spinner_dropdown_item);
+//        dropDownCategory.setAdapter(adapter);
+//        dropDownCategory.setOnItemSelectedListener(this);
+//
+//        feedbackText = findViewById(R.id.feedbackEditText);
+//
+//        sendFeedbackBtn = findViewById(R.id.feedbackButton);
+//        sendFeedbackBtn.setOnClickListener(this);
+//
+//    }
+//    String categoryText;
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//       categoryText = parent.getItemAtPosition(position).toString();
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//
+//    }
+//
+//
+//    @Override
+//    public void onClick(View v) {
+//
+//        if (categoryText.equals(getResources().getString(R.string.feedback_choose_category))) {
+//            sendFeedbackBtn.setError("Please choose a category in the drop down menu");
+//            Toast.makeText(this, sendFeedbackBtn.getError(), Toast.LENGTH_LONG).show();
+//        } else {
+//            backend.postFeedback(categoryText, feedbackText.getText().toString());
+//        }
+//
+//
+//
+//
+//
+//    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +80,27 @@ public class ActivityFeedback extends AppCompatActivity implements AdapterView.O
         setContentView(R.layout.activity_feedback);
 
 
-        dropDownCategory = findViewById(R.id.feedbackDropDownMenu);
-        ArrayAdapter<CharSequence> adapter =
-                ArrayAdapter.createFromResource(this, R.array.feedback_categories_array,
-                        R.layout.support_simple_spinner_dropdown_item);
-        dropDownCategory.setAdapter(adapter);
-        dropDownCategory.setOnItemSelectedListener(this);
-
-        feedbackText = findViewById(R.id.feedbackEditText);
-
-        sendFeedbackBtn = findViewById(R.id.feedbackButton);
-        sendFeedbackBtn.setOnClickListener(this);
-
     }
-    String categoryText;
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-       categoryText = parent.getItemAtPosition(position).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
 
     @Override
     public void onClick(View v) {
+        navToFacebookPage("1014970751849130");
+    }
 
-        if (categoryText.equals(getResources().getString(R.string.feedback_choose_category))) {
-            sendFeedbackBtn.setError("Please choose a category in the drop down menu");
-            Toast.makeText(this, sendFeedbackBtn.getError(), Toast.LENGTH_LONG).show();
-        } else {
-            backend.postFeedback(categoryText, feedbackText.getText().toString());
+
+    public void navToFacebookPage(String id) {
+        try {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://groups/" + id));
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/groups/" + id));
+            startActivity(i);
         }
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {    }
 }
