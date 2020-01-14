73edbbe1fc0bdb30922aa254dc637a6ccaac9546
airo <valdemar_hh@hotmail.com>
2020-01-14 14:30:10 +0100

    Parking Alarm State added: + async call to db + back end + live data + map state -Call from UI -DAO setup

---
+++ app/src/main/java/com/example/p_kontrol/Backend/NetworkAsyncCalls/AsyncGetPVagter.java
@@ -0,0 +1,50 @@
+package com.example.p_kontrol.Backend.NetworkAsyncCalls;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.p_kontrol.Backend.IDatabase;
+import com.example.p_kontrol.Backend.IOnTaskComplete;
+import com.example.p_kontrol.DataBase.FirestoreDAO;
+import com.example.p_kontrol.DataTypes.ATipDTO;
+import com.example.p_kontrol.DataTypes.PVagtDTO;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class AsyncGetPVagter extends AsyncTask<Void, Void, Void> {
+
+    private static final String TAG = "AsyncGetPVagt";
+    private IDatabase DAO = new FirestoreDAO();
+    private double radius;
+    private LatLng location;
+    private MutableLiveData<List<PVagtDTO>> PVagtList;
+
+
+    public AsyncGetPVagter(LatLng location, double radius, MutableLiveData<List<PVagtDTO>> PVagtList) {
+        this.location = location;
+        this.radius = radius;
+        this.PVagtList = PVagtList;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+
+          //  DAO.((PVAGT BY LOCATION))(location, radius, PVagtList );
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "doInBackground: ", e);
+        }
+
+        return null;
+    }
+
+    protected void onPostExecute(List<ATipDTO> result) {
+
+    }
+}
+
--- app/src/main/java/com/example/p_kontrol/UI/ViewModelLiveData/LiveDataViewModel.java
+++ app/src/main/java/com/example/p_kontrol/UI/ViewModelLiveData/LiveDataViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.p_kontrol.Backend.Backend;
 import com.example.p_kontrol.DataTypes.ATipDTO;
+import com.example.p_kontrol.DataTypes.PVagtDTO;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class LiveDataViewModel extends ViewModel {
 
     private MutableLiveData<List<ATipDTO>> tipList;
+    private MutableLiveData<List<PVagtDTO>> pVagtList;
 
     private Backend bk = Backend.getBackend();
 
@@ -20,7 +22,15 @@ public class LiveDataViewModel extends ViewModel {
         bk.getTips(location, tipList);
     }
 
+    public void updatePVagter(LatLng location){
+        bk.getPVagter(location, pVagtList);
+    }
+
     public LiveData<List<ATipDTO>> getTipList() {
         return tipList;
+    }
+
+    public LiveData<List<PVagtDTO>> getPvagtList() {
+        return pVagtList;
     }
 }
